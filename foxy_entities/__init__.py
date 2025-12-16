19ec3eaa91205b2aeb5950741f09cdf2a2af1e66
Dima Shapranov <shapranov.work@gmail.com>
2025-12-16 13:03:40 +0300

    add import exceptions

--- foxy_entities/__init__.py
+++ foxy_entities/__init__.py
@@ -1,4 +1,15 @@
 from foxy_entities.abc import SocialMediaEntity
+from foxy_entities.exceptions import (
+    EntityTypeException,
+    PresenceObjectException,
+    BanningAbcClass,
+)
 from foxy_entities.management_entity import EntitiesController
 
-__all__ = ["SocialMediaEntity", "EntitiesController"]
+__all__ = [
+    "SocialMediaEntity",
+    "EntitiesController",
+    "EntityTypeException",
+    "PresenceObjectException",
+    "BanningAbcClass",
+]
